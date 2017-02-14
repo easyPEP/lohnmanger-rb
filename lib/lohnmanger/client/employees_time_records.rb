629949f6163d7e3bcfb5404f5908ffad1cb23755
kalle saas <kalle@easypep.de>
2017-02-14 10:37:35 +0100

    Add EmployeesTimeRecords and specs

--- lib/lohnmanger/client/employees_time_records.rb
+++ lib/lohnmanger/client/employees_time_records.rb
@@ -1,29 +1,30 @@
 module Lohnmanger
   class Client
     module EmployeesTimeRecords
-      # [
+      # "TimeRecords":[
       #   {
+      #     "EmployeeNo":5,
+      #     "RecordDate":"\/Date(1480546800000+0100)\/",
+      #     "Shift1_End":"PT2H25M",
+      #     "Shift1_Start":"PT1H10M",
+      #     "Shift2_End":"PT23H30M",
+      #     "Shift2_Start":"PT22H23M",
       #     "Break":null,
+      #     "RecordType":0,
       #     "Credit":null,
       #     "CreditDay":null,
-      #     "EmployeeNo":5,
       #     "ExpensesCredit":null,
       #     "FortyFourEuro":null,
       #     "Holiday":null,
       #     "Meals":null,
-      #     "RecordDate":"\/Date(1480546800000+0100)\/",
-      #     "RecordType":0,
-      #     "Shift1_Start":"PT1H10M",
-      #     "Shift1_End":"PT2H25M",
-      #     "Shift2_Start":"PT22H23M",
-      #     "Shift2_End":"PT23H30M",
       #     "TimeAccountBalance":null,
       #     "VariableWageType1":null,
       #     "VariableWageType2":null
       #   }
       # ]
+      # options => Array[]
       def create_employees_time_records(options = {})
-        result = post('UpdateEmployeesTimeRecords', {TimeRecords: options})
+        result = post('UpdateEmployeesTimeRecords', {TimeRecords: Lohnmanger::DateTimeTranslator.json_time_records_to_wcf(options)})
         result['UpdateEmployeesTimeRecordsResult']
       end
 
--- test/lohnmanager_test.rb
+++ test/lohnmanager_test.rb
@@ -40,4 +40,3 @@ class LohnmangerTest < Minitest::Test
   end
 
 end
-
